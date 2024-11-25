b7b3cf871eaeb3a6d20179e60243dccd4038c56a
kogasaa <92961177+kogasaa@users.noreply.github.com>
2024-11-24 19:00:26 -0700

    add a button list in the each ui

--- craftcharacter.h
+++ craftcharacter.h
@@ -2,18 +2,26 @@
 #define CRAFTCHARACTER_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include "model.h"
 
+QT_BEGIN_NAMESPACE
 namespace Ui {
 class craftCharacter;
 }
+QT_END_NAMESPACE
 
 class craftCharacter : public QMainWindow
 {
     Q_OBJECT
+    QList<QPushButton *> allCharacters;
+    QPushButton* lastButtonSelected;
 
 public:
     explicit craftCharacter(QWidget *parent = nullptr);
     ~craftCharacter();
+
+public:
 
 private:
     Ui::craftCharacter *ui;
